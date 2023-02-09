bc7576de08f9e6995874e9d273363ba604063609
pravinpatke37@gmail.com <pravinptke37@gmail.com>
2023-02-09 16:56:22 +0530

    findMinList

---
+++ findMinList.py
@@ -0,0 +1,19 @@
+list = [-1,2,3,4,-2,0]
+#min = list[0]
+#max = list[0]
+newl = []
+while list:
+    min = list[0]
+    print(min)
+    for i in range(len(list)):
+        if list[i] < min:
+           min = list[i]
+	   print(min)
+    newl.append(min)
+    list.remove(min)
+#    if list[i] > max:
+#       max = list [i]
+#print ("Min and Max valu is", min,max)
+print ("New List",newl)
+
+
